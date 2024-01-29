1f7efd0b15b906b9e0c9d85f581a151592252257
TaehongKim <walckm11@gmail.com>
2024-01-29 22:15:47 +0900

    [캐릭터] 플레이어 이동 애니메이션 적용

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,16 +12,30 @@ public class Player : MonoBehaviour
     public float moveSpeed;
     
     private Rigidbody2D rigid2D;
+    private SpriteRenderer spriter;
+    private Animator animator;
 
     private void Start()
     {
         rigid2D = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
    
     private void FixedUpdate()
     {
         Vector2 moveVector = inputVector * moveSpeed * Time.fixedDeltaTime;
         rigid2D.MovePosition( rigid2D.position + moveVector );
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat( "MoveSpeed", inputVector.sqrMagnitude );
+
+        if ( inputVector.x != 0f )
+        {
+            spriter.flipX = inputVector.x < 0f;
+        }
     }
 
     private void OnMove( InputValue _value )
