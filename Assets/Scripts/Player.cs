7332abe870417e3ff8ad1b29b39dc6fee65cec06
TaehongKim <walckm11@gmail.com>
2024-01-29 21:45:32 +0900

    [캐릭터] 플레이어 이동 InputAction 적용, Sprite 메타파일 갱신

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 public class Player : MonoBehaviour
 {
-    public Vector2 inputVec;
+    public Vector2 inputVector;
     public float moveSpeed;
     
     private Rigidbody2D rigid2D;
@@ -15,16 +17,15 @@ public class Player : MonoBehaviour
     {
         rigid2D = GetComponent<Rigidbody2D>();
     }
-    
-    private void Update()
-    {
-        inputVec.x = Input.GetAxisRaw( "Horizontal" );
-        inputVec.y = Input.GetAxisRaw( "Vertical" );
-    }
-
+   
     private void FixedUpdate()
     {
-        Vector2 moveVector = inputVec.normalized * moveSpeed * Time.fixedDeltaTime;
+        Vector2 moveVector = inputVector * moveSpeed * Time.fixedDeltaTime;
         rigid2D.MovePosition( rigid2D.position + moveVector );
+    }
+
+    private void OnMove( InputValue _value )
+    {
+        inputVector = _value.Get<Vector2>();
     }
 }
