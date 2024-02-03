793b59b4eb46701e51a3c3797c3ab6354e6b81ed
TaehongKim <walckm11@gmail.com>
2024-02-03 16:32:54 +0900

    [캐릭터] Enemy 일부 데이터 ScriptableObject화

--- Assets/Scripts/Character/Enemy.cs
+++ Assets/Scripts/Character/Enemy.cs
@@ -6,9 +6,23 @@ using UnityEngine;
 public class Enemy : PoolObject
 {
     [SerializeField]
+    private EnemyData enemyData;
+
+    private float curHp;
+    public float CurHp
+    {
+        get { return curHp; }
+        private set
+        {
+            curHp = Mathf.Min( value, enemyData.maxHp );
+            if ( curHp <= 0 )
+            {
+                OnDead();
+            }
+        }
+    }
+
     private Rigidbody2D target;
-    [SerializeField]
-    private float moveSpeed;
 
     private Rigidbody2D rigid2D;
     private SpriteRenderer spriter;
@@ -19,13 +33,13 @@ public class Enemy : PoolObject
         rigid2D = GetComponent<Rigidbody2D>();
         spriter = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-
+        
         target = GameManager.Inst.player.GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        Vector2 movePosition = ( target.position - rigid2D.position ).normalized * moveSpeed * Time.fixedDeltaTime;
+        Vector2 movePosition = ( target.position - rigid2D.position ).normalized * enemyData.moveSpeed * Time.fixedDeltaTime;
         rigid2D.MovePosition( rigid2D.position + movePosition );
 
         spriter.flipX = rigid2D.position.x > target.position.x;
@@ -38,5 +52,11 @@ public class Enemy : PoolObject
             Vector2 newPos = ( _other.gameObject.transform.position - transform.position ) * 2;
             transform.Translate( newPos );
         }
+    }
+
+    private void OnDead()
+    {
+        /// +시체 처리, 점수 처리 등
+        Release();
     }
 }
---
+++ Assets/Scripts/Character/EnemyData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+[CreateAssetMenu( fileName = "EnemyData", menuName = "Scriptable Objects/EnemyData" )]
+public class EnemyData : ScriptableObject
+{
+    public float maxHp;
+    public float moveSpeed;
+    public float attackDamage;
+}
