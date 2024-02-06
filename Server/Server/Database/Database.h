6196f2855a708319cd5b56a19c69413384d8999c
wns <ashi0@naver.com>
2024-02-06 19:33:39 +0900

    [서버] DB 라이브러리 교체 - DB 연결이 안되던 문제 수정

--- Server/Server/Database/Database.h
+++ Server/Server/Database/Database.h
@@ -1,25 +1,11 @@
 #pragma once
-//#include "Global/Header.h"
+#include "mysql.h"
 #include "Global/Singleton.hpp"
-
-#include "mysql/jdbc.h"
-
-//#include "jdbc/cppconn/driver.h"
-//#include "jdbc/cppconn/connection.h"
-//#include "jdbc/cppconn/statement.h"
-//#include "jdbc/cppconn/resultset.h"
-//#include "jdbc/cppconn/sqlstring.h"
-//#include "jdbc/cppconn/exception.h"
 
 class Database : public Singleton<Database>
 {
 private:
-	sql::Driver* driver;
-	sql::Connection* connection;
-	sql::Statement* statement;
-	sql::ResultSet* result;
 
-	//std::mutex mtx;
 
 public:
 	bool Initialize();
--- Server/Server/Run/Server.cpp
+++ Server/Server/Run/Server.cpp
@@ -1,6 +1,7 @@
 #include "Server.h"
 #include "Managed/PacketSystem.h"
 #include "Managed/IOCP.h"
+#include "Database/Database.h"
 
 Server::Server()
 {
@@ -9,19 +10,24 @@ Server::Server()
 
 void Server::Start( const int _port, const char* _address )
 {
-	if ( IOCP::Inst().Initialize() == false )
+	if ( !IOCP::Inst().Initialize() )
 	{
 		std::cout << "IOCP thread failed" << std::endl;
 	}
 
-	if ( PacketSystem::Inst().Initialize() == false )
+	if ( !PacketSystem::Inst().Initialize() )
 	{
 		std::cout << "Packet processing failed" << std::endl;
 	}
 
-	if ( acceptor.Accept( _port, _address ) == false )
+	if ( !acceptor.Accept( _port, _address ) )
 	{
 		std::cout << "Accept failed" << std::endl;
+	}
+
+	if ( Database::Inst().Initialize() )
+	{
+
 	}
 
 	if ( ::WaitForSingleObject( kill, INFINITE ) == WAIT_FAILED )
