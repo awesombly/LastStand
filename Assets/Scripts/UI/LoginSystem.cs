8e86f0e0476f78a103c8eea36071bf3602b90c8a
wns <ashi0@naver.com>
2024-02-08 02:42:14 +0900

    [UI] 로그인 요청 후 처리할 UI 제작 - 로그인 실패 문구 등.

--- Assets/Scripts/UI/LoginSystem.cs
+++ Assets/Scripts/UI/LoginSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,25 @@ using TMPro;
 
 public class LoginSystem : MonoBehaviour
 {
+    [Flags]
+    enum LoginType
+    {
+        Default     = 0,
+        LoginFail   = 1 << 0,
+        AccountFail = 1 << 1,
+    }
     public TMP_InputField email;
     public TMP_InputField password;
 
+    [Header( "Panel" )]
+    public GameObject loginFail;
+    public GameObject AccountFail;
+    LoginType type;
+
     private void Awake()
     {
+        email?.ActivateInputField();
+
         if ( password != null )
              password.contentType = TMP_InputField.ContentType.Password;
     }
@@ -19,22 +34,81 @@ public class LoginSystem : MonoBehaviour
         ProtocolSystem.Inst.Regist( new ResLogin(), ResponseLogin );
     }
 
+    public void ActiveLoginFail( bool _isActive )
+    {
+        type = _isActive ? type |=  LoginType.LoginFail :
+                           type &= ~LoginType.LoginFail;
+
+        loginFail.SetActive( _isActive );
+    }
+
+    public void ActiveAccountFail( bool _isActive )
+    {
+        type = _isActive ? type |=  LoginType.AccountFail :
+                           type &= ~LoginType.AccountFail;
+
+        AccountFail.SetActive( _isActive );
+    }
+
     private void Update()
     {
+        if ( type == LoginType.Default )
+        {
+            if ( Input.GetKeyDown( KeyCode.Tab ) )
+            {
+                if ( email.isFocused )
+                {
+                    email.DeactivateInputField();
+                    password.ActivateInputField();
+                }
+                else
+                {
+                    email.ActivateInputField();
+                    password.DeactivateInputField();
+                }
+            }
+        }
+
         if ( Input.GetKeyDown( KeyCode.Return ) )
         {
-            ReqLogin protocol;
-            protocol.email    = email.text;
-            protocol.password = password.text;
-            Network.Inst.Send( new Packet( protocol ) );
+            if ( type == LoginType.Default )
+            {
+                ReqLogin protocol;
+                protocol.email = email.text;
+                protocol.password = password.text;
+                Network.Inst.Send( new Packet( protocol ) );
+            }
+
+            if ( ( type & LoginType.LoginFail ) != 0 )
+            {
+                ActiveLoginFail( false );
+            }
+
+
+            if ( ( type & LoginType.AccountFail ) != 0 )
+            {
+                ActiveAccountFail( false );
+            }
         }
+    }
+
+    private void ReqAccount ()
+    {
+        // 가입 패킷 보내기
+        // ReqLogin protocol;
+        // protocol.email = email.text;
+        // protocol.password = password.text;
+        // Network.Inst.Send( new Packet( protocol ) );
     }
 
     private void ResponseLogin( Packet _packet )
     {
         var data = Global.FromJson<ResLogin>( _packet );
         if ( data.nickname == string.Empty )
-            Debug.Log( "이메일 또는 비밀번호를 확인해주세요." );
+        {
+            type |= LoginType.LoginFail;
+            loginFail.SetActive( true );
+        }
         else
             Debug.Log( $"{data.nickname} 유저가 입장했습니다." );
     }
