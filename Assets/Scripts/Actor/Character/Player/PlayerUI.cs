e13fae1eb53b8f61195d3eaa0abcc4b85e87e33b
TaehongKim <walckm11@gmail.com>
2024-02-22 19:21:55 +0900

    [버그수정] Player 재장전 동기화 안되는 현상 수정

--- Assets/Scripts/Actor/Character/Player/PlayerUI.cs
+++ Assets/Scripts/Actor/Character/Player/PlayerUI.cs
@@ -4,40 +4,46 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class WeaponStatUi : MonoBehaviour
+public class PlayerUI : MonoBehaviour
 {
     public TextMeshProUGUI magazineText;
     public TextMeshProUGUI ammoText;
     public Slider reloadBar;
 
-    private Weapon equipWeapon;
+    private Player player;
 
     private void Awake()
     {
         reloadBar.gameObject.SetActive( false );
-        
-        GameManager.OnChangeLocalPlayer += OnChangeLocalPlayer;
+        player = GetComponent<Player>();
+        player.OnChangeEquipWeapon += OnChangeEquipWeapon;
+        OnChangeEquipWeapon( null, player.EquipWeapon );
     }
 
     private void OnChangeAmmo( int _old, int _new )
     {
-        ammoText?.SetText( _new.ToString() );
+        if ( !ReferenceEquals( player, null ) && player.IsLocal )
+        {
+            ammoText?.SetText( _new.ToString() );
+        }
     }
 
     private void OnChangeMagazine( int _old, int _new )
     {
-        magazineText?.SetText( _new.ToString() );
+        if ( !ReferenceEquals( player, null ) && player.IsLocal )
+        { 
+            magazineText?.SetText( _new.ToString() );
+        }
     }
 
     private void OnChangeReloadDelay( float _old, float _new )
     {
-        reloadBar.gameObject.SetActive( !equipWeapon.stat.reloadDelay.IsZero );
-        reloadBar.value = ( equipWeapon.stat.reloadDelay.Max - equipWeapon.stat.reloadDelay.Current ) / equipWeapon.stat.reloadDelay.Max;
+        reloadBar.gameObject.SetActive( !player.EquipWeapon.stat.reloadDelay.IsZero );
+        reloadBar.value = ( player.EquipWeapon.stat.reloadDelay.Max - player.EquipWeapon.stat.reloadDelay.Current ) / player.EquipWeapon.stat.reloadDelay.Max;
     }
 
     private void OnChangeEquipWeapon( Weapon _old, Weapon _new )
     {
-        equipWeapon = _new;
         if ( _old == _new )
         {
             return;
@@ -60,31 +66,5 @@ public class WeaponStatUi : MonoBehaviour
             OnChangeMagazine( 0, _new.stat.magazine.Current );
             OnChangeReloadDelay( 0f, _new.stat.reloadDelay.Current );
         }
-        else
-        {
-            OnChangeAmmo( 0, 0 );
-            OnChangeMagazine( 0, 0 );
-        }
-    }
-
-    private void OnChangeLocalPlayer( Player _old, Player _new )
-    {
-        Weapon oldWeapon = null;
-        Weapon newWeapon = null;
-        if ( _old != null )
-        {
-            oldWeapon = _old.EquipWeapon;
-            _old.OnChangeEquipWeapon -= OnChangeEquipWeapon;
-        }
-
-        if ( _new != null )
-        {
-            newWeapon = _new.EquipWeapon;
-            _new.OnChangeEquipWeapon += OnChangeEquipWeapon;
-        }
-
-        OnChangeEquipWeapon( oldWeapon, newWeapon );
-
-        /// TODO: reloadBar 교체 필요
     }
 }
