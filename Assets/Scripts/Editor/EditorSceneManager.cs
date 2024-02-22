150b7a580808ad814f05adbfc4f6cec22f23d9f4
TaehongKim <walckm11@gmail.com>
2024-02-23 08:32:37 +0900

    [에디터] 특정 씬에서 시작할 수 있는 상단 메뉴 추가 (MyEditor)

---
+++ Assets/Scripts/Editor/EditorSceneManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class EditorSceneUtil : MonoBehaviour
+{
+    [MenuItem( "MyEditor/CurrentScene Start" )]
+    public static void StartCurrentScene()
+    {
+        EditorSceneManager.playModeStartScene = null;
+        UnityEditor.EditorApplication.isPlaying = true;
+    }
+
+    [MenuItem( "MyEditor/LoginScene Start" )]
+    public static void SetupLoginScene()
+    {
+        StartScene( SceneType.Login );
+    }
+
+    [MenuItem( "MyEditor/LobbyScene Start" )]
+    public static void SetupLobbyScene()
+    {
+        StartScene( SceneType.Lobby );
+    }
+
+    private static void StartScene( SceneType _sceneType, Network.IpType _ipType = Network.IpType.NONE )
+    {
+        string scenePath = null;
+        foreach ( var scene in EditorBuildSettings.scenes )
+        {
+            string sceneName = _sceneType.ToString();
+            if ( scene.path.Contains( sceneName ) )
+            {
+                scenePath = scene.path;
+                break;
+            }
+        }
+
+        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>( scenePath );
+        EditorSceneManager.playModeStartScene = sceneAsset;
+        UnityEditor.EditorApplication.isPlaying = true;
+    }
+}
