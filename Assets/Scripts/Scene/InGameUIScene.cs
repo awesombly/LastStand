cde8f92f5aac218a6fa479600eda40f6e13c3b69
wns <ashi0@naver.com>
2024-02-23 11:51:52 +0900

    [기타] Sample 클래스안의 기능 UIScene으로 이동

--- Assets/Scripts/Scene/InGameUIScene.cs
+++ Assets/Scripts/Scene/InGameUIScene.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
+using static PacketType;
+
 public class InGameUIScene : SceneBase
 {
     public Camera uiCamera;
@@ -10,14 +12,45 @@ public class InGameUIScene : SceneBase
     public TextMeshProUGUI magazineText;
     public TextMeshProUGUI ammoText;
 
+    public GameObject pause;
+    private bool isProgress;
+
     protected override void Awake()
     {
         base.Awake();
         SceneType = SceneType.InGame_UI;
         uiCamera.clearFlags = CameraClearFlags.Depth;
+
+        ProtocolSystem.Inst.Regist( EXIT_STAGE_ACK, AckExitStage );
     }
+
     protected override void Start()
     {
         base.Start();
+    }
+
+    private void Update()
+    {
+        if ( Input.GetKeyDown( KeyCode.Escape ) )
+        {
+            if ( pause.activeInHierarchy ) pause.SetActive( false );
+            else pause.SetActive( true );
+        }
+    }
+
+    public void ReqExitStage()
+    {
+        if ( isProgress || StageSystem.Info == null )
+            return;
+
+        isProgress = true;
+        Network.Inst.Send( EXIT_STAGE_REQ, StageSystem.Info.Value );
+    }
+
+    private void AckExitStage( Packet _packet )
+    {
+        isProgress = false;
+        StageSystem.Info = null;
+        SceneBase.LoadScene( SceneType.Lobby );
     }
 }
