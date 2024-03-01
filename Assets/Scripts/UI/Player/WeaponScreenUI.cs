cd5aa2001f2ce2eaa5f3db12e0e75cefba84487c
TaehongKim <walckm11@gmail.com>
2024-03-01 11:43:38 +0900

    [UI] Player의 남은 탄창 표시 UI 작업

--- Assets/Scripts/UI/Player/WeaponScreenUI.cs
+++ Assets/Scripts/UI/Player/WeaponScreenUI.cs
@@ -9,9 +9,41 @@ public class WeaponScreenUI : MonoBehaviour
     [SerializeField]
     private Image weaponImage;
 
+    [SerializeField]
+    private GameObject magazinePanel;
+    [SerializeField]
+    private Image bulletIconPrefab;
+    private List<Image> bulletIcons = new List<Image>();
+    private WNS.ObjectPool<Image> bulletIconPool;
+
+    private Weapon equipWeapon;
+
     private void Awake()
     {
+        bulletIconPool = new WNS.ObjectPool<Image>( bulletIconPrefab, magazinePanel.transform );
+
         GameManager.OnChangeLocalPlayer += OnChangeLocalPlayer;
+    }
+
+    private void UpdateBulletIcons()
+    {
+        if ( ReferenceEquals( equipWeapon, null ) )
+        {
+            return;
+        }
+
+        int usedMagazine = equipWeapon.myStat.magazine.Max - equipWeapon.myStat.magazine.Current;
+        for ( int i = 0; i < bulletIcons.Count; ++i )
+        {
+            if ( usedMagazine <= i )
+            {
+                bulletIcons[i].color = Color.white;
+            }
+            else
+            {
+                bulletIcons[i].color = Color.gray;
+            }
+        }
     }
 
     private void OnChangeLocalPlayer( Player _old, Player _new )
@@ -21,12 +53,12 @@ public class WeaponScreenUI : MonoBehaviour
             return;
         }
 
-        if ( _old != null )
+        if ( !ReferenceEquals( _old, null ) )
         {
             _old.OnChangeEquipWeapon -= OnChangeEquipWeapon;
         }
 
-        if ( _new != null )
+        if ( !ReferenceEquals( _new, null ) )
         {
             _new.OnChangeEquipWeapon += OnChangeEquipWeapon;
         }
@@ -34,11 +66,43 @@ public class WeaponScreenUI : MonoBehaviour
 
     private void OnChangeEquipWeapon( Weapon _old, Weapon _new )
     {
-        if ( _old == _new || ReferenceEquals( _new, null ) )
+        if ( _old == _new )
         {
             return;
         }
 
-        weaponImage.sprite = _new.GetComponent<SpriteRenderer>().sprite;
+        if ( !ReferenceEquals( _old, null ) )
+        {
+            _old.myStat.magazine.OnChangeCurrent -= OnChangeMagazine;
+        }
+
+        if ( ReferenceEquals( _new, null ) )
+        {
+            return;
+        }
+        equipWeapon = _new;
+        equipWeapon.myStat.magazine.OnChangeCurrent += OnChangeMagazine;
+
+        // Weapon Image
+        weaponImage.sprite = equipWeapon.GetComponent<SpriteRenderer>().sprite;
+
+        // Bullet Icons
+        bulletIcons.Clear();
+        bulletIconPool.AllDespawn();
+
+        for ( int i = 0; i < equipWeapon.myStat.magazine.Max; ++i )
+        {
+            Image bullet = bulletIconPool.Spawn();
+            bullet.transform.SetSiblingIndex( i );
+            bulletIcons.Add( bullet );
+        }
+        UpdateBulletIcons();
+        // 처음 접속시 업데이트 안되는 이슈가 있어 추가
+        LayoutRebuilder.ForceRebuildLayoutImmediate( magazinePanel.GetComponent<RectTransform>() );
+    }
+
+    private void OnChangeMagazine( int _old, int _new )
+    {
+        UpdateBulletIcons();
     }
 }
