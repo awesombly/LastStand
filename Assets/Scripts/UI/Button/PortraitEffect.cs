768508b2f17168d4cc6b4115f9b6b0213b407209
wns <ashi0@naver.com>
2024-03-04 21:37:25 +0900

    [UI] 캐릭터 선택 이펙트 추가

---
+++ Assets/Scripts/UI/Button/PortraitEffect.cs
@@ -0,0 +1,32 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent( typeof( SpriteRenderer ) )]
+public class PortraitEffect : MonoBehaviour
+{
+    private Animator anim;
+    private SpriteRenderer rdr;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rdr = GetComponent<SpriteRenderer>();
+        rdr.color = Color.gray;
+    }
+
+    public void OnPointerEnter()
+    {
+        anim.SetBool( "IsHover", true );
+        rdr.DOColor( Color.white, .35f );
+        transform.DOScale( 1.25f, .35f );
+    }
+
+    public void OnPointerExit()
+    {
+        anim.SetBool( "IsHover", false );
+        rdr.DOColor( Color.gray, .35f );
+        transform.DOScale( 1f, .35f );
+    }
+}
