00a09225cd3b358af8b13473f75a5d3ca5a4ce44
TaehongKim <walckm11@gmail.com>
2024-03-06 21:05:06 +0900

    [카메라] 카메라 줌 기능 추가. (마우스 휠)

--- Assets/Scripts/Actor/Character/Player/ActionReceiver.cs
+++ Assets/Scripts/Actor/Character/Player/ActionReceiver.cs
@@ -1,8 +1,10 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.U2D;
 
 public class ActionReceiver : MonoBehaviour
 {
@@ -16,6 +18,65 @@ public class ActionReceiver : MonoBehaviour
     public event Action OnDodgeEvent;
     public event Action<int/*index*/> OnSwapWeaponEvent;
     public event Action OnInteractionEvent;
+
+    #region Camera
+    private float targetOrthoSize;
+    private float prevOrthoSize;
+    private Coroutine orthoCoroutine = null;
+
+    private PixelPerfectCamera ppCamera;
+    private CinemachineVirtualCamera virtualCamera;
+    private Cinemachine.CinemachinePixelPerfect pixelPerfect;
+    private CinemachineConfiner2D confiner2D;
+    #endregion
+
+    private void Awake()
+    {
+        ppCamera = Camera.main.GetComponent<PixelPerfectCamera>();
+        virtualCamera = Camera.main.GetComponent<CinemachineBrain>().ActiveVirtualCamera as CinemachineVirtualCamera;
+        pixelPerfect = virtualCamera.GetComponent<Cinemachine.CinemachinePixelPerfect>();
+        confiner2D = virtualCamera.GetComponent<Cinemachine.CinemachineConfiner2D>();
+        if ( ppCamera is null || virtualCamera is null || pixelPerfect is null )
+        {
+            Debug.LogError( $"Not Exist Components. {ppCamera}, {virtualCamera}, {pixelPerfect}" );
+        }
+
+        targetOrthoSize = virtualCamera.m_Lens.OrthographicSize;
+        prevOrthoSize = Camera.main.orthographicSize;
+    }
+
+    private void FixedUpdate()
+    {
+        // CinemachinePixelPerpect 사용시 카메라 OrthoSize가 일정 구간이 아니면 바뀌지 않는다.(픽셀 보정용인듯)
+        // 그대로 OrthoSize 변경시 끊기는 느낌이 들어, 바뀌는 구간마다 보간해준다
+        float correctSize = ppCamera.CorrectCinemachineOrthoSize( targetOrthoSize );
+        if ( !Mathf.Approximately( prevOrthoSize, correctSize ) )
+        {
+            if ( orthoCoroutine is not null )
+            {
+                StopCoroutine( orthoCoroutine );
+            }
+            prevOrthoSize = correctSize;
+            orthoCoroutine = StartCoroutine( LerpCameraOrthoSize( correctSize ) );
+        }
+    }
+
+    private IEnumerator LerpCameraOrthoSize( float _targetSize )
+    {
+        pixelPerfect.enabled = false;
+        float orthoSize = virtualCamera.m_Lens.OrthographicSize;
+
+        while ( !Mathf.Approximately( orthoSize, _targetSize ) )
+        {
+            orthoSize = Mathf.MoveTowards( orthoSize, _targetSize, Time.deltaTime * 3f );
+            virtualCamera.m_Lens.OrthographicSize = orthoSize;
+            // OrthoSize 변경시 충돌경계 업데이트가 안되어 추가
+            confiner2D.InvalidateCache();
+            yield return YieldCache.WaitForEndOfFrame;
+        }
+
+        pixelPerfect.enabled = true;
+    }
 
     private void OnDisable()
     {
@@ -62,6 +123,11 @@ public class ActionReceiver : MonoBehaviour
     private void OnInteraction()
     {
         OnInteractionEvent?.Invoke();
+    }
+
+    private void OnCameraZoom( InputValue _value )
+    {
+        targetOrthoSize = Mathf.Clamp( targetOrthoSize + _value.Get<float>() * 0.01f, 1f, 15f );
     }
     #endregion
 }
