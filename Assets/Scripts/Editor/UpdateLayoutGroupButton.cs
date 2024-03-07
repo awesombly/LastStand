6967ff337680de76cf026f0aa3b99aaee32bca8d
wns <ashi0@naver.com>
2024-03-07 19:33:47 +0900

    [기타] 커스텀 LayoutGroup 인스펙터안에 디버깅용 Update 버튼 생성

---
+++ Assets/Scripts/Editor/UpdateLayoutGroupButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor( typeof( CustomVerticalLayoutGroup ) )]
+public class UpdateLayoutGroupButton : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var group = target as CustomLayoutGroup;
+        if ( GUILayout.Button( "Update", GUILayout.Width( 100 ), GUILayout.Height( 25 ) ) )
+             group.UpdateLayoutGroup();
+    }
+}
+
+[CustomEditor( typeof( CustomHorizontalLayoutGroup ) )]
+public class UpdateHorizontalLayoutGroup : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var group = target as CustomLayoutGroup;
+        if ( GUILayout.Button( "Update", GUILayout.Width( 100 ), GUILayout.Height( 25 ) ) )
+             group.UpdateLayoutGroup();
+    }
+}
