b5b48ec4e3a9c5f3b7359def1cb79410261fd061
wns <ashi0@naver.com>
2024-03-12 18:21:09 +0900

    [UI] 옵션 기능에 마우스 위치할 때 백그라운드 이미지 활성화되도록 개선

---
+++ Assets/Scripts/UI/Button/ButtonHoverEffect.cs
@@ -0,0 +1,37 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+
+[RequireComponent( typeof( Image ) )]
+public class ButtonHoverEffect : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Image image;
+    public float targetAlpha = .015f;
+    public float duration    = .25f;
+
+    private void Awake()
+    {
+        if ( TryGetComponent( out image ) )
+        {
+            image.maskable      = false;
+            image.raycastTarget = false;
+
+            image.color = new Color( 1f, 1f, 1f, 0f );
+        }
+    }
+
+    public void OnPointerEnter( PointerEventData eventData )
+    {
+        image.DOFade( targetAlpha, duration );
+    }
+
+    public void OnPointerExit( PointerEventData eventData )
+    {
+        image.DOFade( 0f, duration );
+    }
+}
