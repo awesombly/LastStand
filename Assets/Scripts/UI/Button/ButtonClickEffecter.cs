bfcad3bd925dd60a0f49d77d301d57f52a5f5222
TaehongKim <walckm11@gmail.com>
2024-03-22 09:06:48 +0900

    [UI] 모바일용 가상패드 버튼 클릭시 효과 추가

---
+++ Assets/Scripts/UI/Button/ButtonClickEffecter.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonClickEffecter : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] float duration;
+    [Range( 1f, 2f )]
+    [SerializeField] float scaleMultiplier;
+    [SerializeField] Image targetImage;
+    [SerializeField] Color targetColor;
+
+    private Vector3 targetSize;
+    private Vector3 originScale;
+    private Color originColor;
+    private Sequence sequence;
+
+    private void Awake()
+    {
+        targetSize = new Vector3( scaleMultiplier, scaleMultiplier, 1f );
+
+        originScale = transform.localScale;
+        originColor = targetImage.color;
+    }
+
+    private void Start()
+    {
+        sequence = DOTween.Sequence().Pause().SetAutoKill( false );
+        sequence.Append( transform.DOScale( targetSize, duration ) )
+            .Join( targetImage.DOColor( targetColor, duration ) )
+            .OnComplete( () =>
+            {
+                transform.localScale = originScale;
+                targetImage.color = originColor;
+            } );
+    }
+
+    private void OnDestroy()
+    {
+        DOTween.Kill( sequence );
+    }
+
+    public void OnPointerClick( PointerEventData eventData )
+    {
+        transform.localScale = originScale;
+        targetImage.color = originColor;
+        sequence.Restart();
+    }
+}
